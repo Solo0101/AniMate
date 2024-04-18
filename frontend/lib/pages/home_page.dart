3c00388f3805488426aaa45c6c56b7b9a6ebc618
Robert Vinițchi <robertvinitchi@gmail.com>
2024-04-18 22:04:37 +0300

    made page scrollable

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -12,7 +12,7 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   final double topContainerPercentage =
-  0.3; //bottom percentage will be the rest of the page
+      0.3; //bottom percentage will be the rest of the page
   final double profileHeight = 120;
 
   @override
@@ -29,61 +29,84 @@ class HomePage extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       body: Container(
         color: backgroundColor,
-        child: Column(
-          children: <Widget>[
-            Stack(alignment: Alignment.center,
-                clipBehavior: Clip.none,
-                children: [
-                  Container(
-                    height: topContainerHeight,
-                    color: primaryGreen,
-                    child: Center(
-                      child: SafeArea(
-                        child: Column(
-                          // mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                              height: (topContainerHeight - profileHeight / 2) - profileHeight + 10,
-                            ),
-                            const Text('Home',
-                                style: TextStyle(
-                                    fontSize: 40.0, color: primaryTextColor)),
-                          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Stack(
+                  alignment: Alignment.center,
+                  clipBehavior: Clip.none,
+                  children: [
+                    Container(
+                      height: topContainerHeight,
+                      color: primaryGreen,
+                      child: Center(
+                        child: SafeArea(
+                          child: Column(
+                            // mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                height:
+                                    (topContainerHeight - profileHeight / 2) -
+                                        profileHeight +
+                                        10,
+                              ),
+                              const Text('Home',
+                                  style: TextStyle(
+                                      fontSize: 40.0, color: primaryTextColor)),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Positioned(
-                    top: top,
-                    child: CircleAvatar(
-                      radius: profileHeight / 2,
-                      backgroundColor: Colors.grey,
-                      child: Icon(
-                        Icons.person,
-                        size: profileHeight,
-                        color: Colors.black,
+                    Positioned(
+                      top: top,
+                      child: CircleAvatar(
+                        radius: profileHeight / 2,
+                        backgroundColor: Colors.grey,
+                        child: Icon(
+                          Icons.person,
+                          size: profileHeight,
+                          color: Colors.black,
+                        ),
                       ),
-                    ),
-                  )
-                ]),
+                    )
+                  ]),
               SizedBox(height: profileHeight / 2 + 20),
-             Padding(
-               padding: EdgeInsets.symmetric(horizontal: screenSizeWidth * 0.1),
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Wrap(
-                    children: [
-                      PetIcon(petIconImage: 'lib/src/images/alb.jpeg', petName: 'petName', widget: PetProfilePage()),
-                      PetIcon(petIconImage: 'lib/src/images/dobberman.jpeg', petName: 'petName', widget: PetProfilePage()),
-                      PetIcon(petIconImage: 'lib/src/images/idk.jpeg', petName: 'petName', widget: PetProfilePage()),
-                      PetIcon(petIconImage: 'lib/src/images/labrador.jpeg', petName: 'petName', widget: PetProfilePage()),
-                    ],
-                  ),
-                ],
-              ),
-            )
-          ],
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: screenSizeWidth * 0.1),
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Wrap(
+                      children: [
+                        PetIcon(
+                            petIconImage: 'lib/src/images/alb.jpeg',
+                            petName: 'petName',
+                            widget: PetProfilePage()),
+                        PetIcon(
+                            petIconImage: 'lib/src/images/dobberman.jpeg',
+                            petName: 'petName',
+                            widget: PetProfilePage()),
+                        PetIcon(
+                            petIconImage: 'lib/src/images/idk.jpeg',
+                            petName: 'petName',
+                            widget: PetProfilePage()),
+                        PetIcon(
+                            petIconImage: 'lib/src/images/labrador.jpeg',
+                            petName: 'petName',
+                            widget: PetProfilePage()),
+                        PetIcon(
+                            petIconImage: 'lib/src/images/alb.jpeg',
+                            petName: 'petName',
+                            widget: PetProfilePage()),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
