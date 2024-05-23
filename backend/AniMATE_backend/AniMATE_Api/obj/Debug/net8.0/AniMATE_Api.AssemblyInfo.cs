5d695e4f071bea41af384a3b0563a5d76642691c
Adrian Solomon <adrian.solomon@student.upt.ro>
2024-05-23 12:39:29 +0300

    test attempt 1

--- backend/AniMATE_backend/AniMATE_Api/obj/Debug/net8.0/AniMATE_Api.AssemblyInfo.cs
+++ backend/AniMATE_backend/AniMATE_Api/obj/Debug/net8.0/AniMATE_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AniMATE_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130df4aedbeeeb25ec01fa3154eb5e17fdf3e9c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1094b579ab7c37776c5aef6330f6be1207d1a26")]
 [assembly: System.Reflection.AssemblyProductAttribute("AniMATE_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AniMATE_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
